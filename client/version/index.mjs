988f73a438e2d15c4641e1e9fe12469ac3973394
Niels Kühnel <nk@relevant-edge.com>
2025-08-26 17:49:06 +0200

    Removed "dead" package.

--- client/version/index.mjs
+++ client/version/index.mjs
@@ -1,4 +1,4 @@
-const CLIENT_SCRIPT_REVISION = "mespo5sr";
+const CLIENT_SCRIPT_REVISION = "mespzn1v";
 const appendClientScriptRevision = (scriptSrc)=>scriptSrc.replace(/^([^?]*)(\?.*?(&)?)?$/g, (_, prefix, query, amp)=>`${prefix}${query ? amp ? "" : "&" : "?"}_rev=${CLIENT_SCRIPT_REVISION}`);
 
 export { CLIENT_SCRIPT_REVISION, appendClientScriptRevision };
--- engine/es/index.mjs
+++ engine/es/index.mjs
@@ -6,7 +6,7 @@ const CLIENT_SCRIPT_QUERY = "opt";
 const EVENT_HUB_QUERY = "var";
 const CONTEXT_NAV_QUERY = "mnt";
 const SCHEMA_TYPES_QUERY = "$types";
-const BUILD_REVISION_QUERY = "rev=" + "mespodgo" ;
+const BUILD_REVISION_QUERY = "rev=" + "mespzu4u" ;
 const SCOPE_INFO_KEY = "@info";
 const CONSENT_INFO_KEY = "@consent";
 const SESSION_REFERENCE_KEY = "@session_reference";
